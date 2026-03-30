282ef5419ffd019af25e26fcbbc086503834eec4
raihanrosadi07 <raihanrosadi284@gmail.com>
2026-03-30 21:52:50 +0700

    main

--- looping.cpp
+++ looping.cpp
@@ -79,3 +79,13 @@ int main(){
 
 #include <iostream>
 using namespace std;
+
+int main(){
+    float nilB,nilM,rerata;
+    string status;
+
+    cout<<"Masukan nilai Matematika = ";
+    cin>>nilM;
+    cout<<"Masukan nilai Bahasa Inggris = ";
+    cin>>nilB;
+}
