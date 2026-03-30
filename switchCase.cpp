384b5e1e9e93d9ffdf1da7a31c6c039f83776d5a
raihanrosadi07 <raihanrosadi284@gmail.com>
2026-03-30 22:22:17 +0700

    main

--- switchCase.cpp
+++ switchCase.cpp
@@ -41,3 +41,11 @@ void output() {
     cout << "Status Kuota    : " << statusKuota << endl;
     cout << "----------------------------------" << endl;
 }
+
+int main() {
+    input();
+    pilihanJurusan();
+    output();
+
+    return 0;
+}
